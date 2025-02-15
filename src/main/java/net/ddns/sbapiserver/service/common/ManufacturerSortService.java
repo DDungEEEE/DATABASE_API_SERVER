c132f7e295fd399898ada3eceace97612b910067
DDungEEEE <thsdnjsdlr12@gmail.com>
2025-02-15 20:58:45 +0900

    ManufacturerSort Service Update

--- src/main/java/net/ddns/sbapiserver/service/common/ManufacturerSortService.java
+++ src/main/java/net/ddns/sbapiserver/service/common/ManufacturerSortService.java
@@ -8,6 +8,7 @@ import net.ddns.sbapiserver.repository.common.ManufacturerSortRepository;
 import net.ddns.sbapiserver.repository.common.ProductsRepository;
 import net.ddns.sbapiserver.service.helper.ServiceErrorHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collections;
 import java.util.Comparator;
@@ -21,11 +22,13 @@ public class ManufacturerSortService {
     private final ServiceErrorHelper serviceErrorHelper;
     private final ProductsRepository productsRepository;
 
+    @Transactional
     public List<ManufacturerSortDto.Result> getAllManufacturerSort(){
         List<ManufacturerSort> allManufacturerSorts = manufacturerSortRepository.findAll();
         return ManufacturerSortDto.Result.of(allManufacturerSorts);
     }
 
+    @Transactional
     public List<ManufacturerSortDto.Result> findManufacturerSortByManufacturerId(int manufacturerId){
         List<ManufacturerSort> all = manufacturerSortRepository.getManufacturerSortByManufacturersManufacturerId(manufacturerId);
 
@@ -36,12 +39,14 @@ public class ManufacturerSortService {
         return ManufacturerSortDto.Result.of(sortedManufacturerSorts);
     }
 
+    @Transactional
     public ManufacturerSortDto.Result createManufacturerSort(ManufacturerSortDto.Create create){
         ManufacturerSort createEntity = create.asEntity(sort -> sort.withManufacturers(serviceErrorHelper.findManufacturerOrElseThrow404(create.getManufacturerId())));
         ManufacturerSort saveEntity = manufacturerSortRepository.save(createEntity);
         return ManufacturerSortDto.Result.of(saveEntity);
     }
 
+    @Transactional
     public ManufacturerSortDto.Result updateManufacturerSort(ManufacturerSortDto.Put put){
         ManufacturerSort putEntity = put.asPutEntity(serviceErrorHelper.findManufacturerSortOrElseThrow404(put.getManufacturerSortId()));
         putEntity = putEntity.withManufacturers(serviceErrorHelper.findManufacturerOrElseThrow404(put.getManufacturerId()));
@@ -49,6 +54,7 @@ public class ManufacturerSortService {
         return ManufacturerSortDto.Result.of(saveEntity);
     }
 
+    @Transactional
     public void deleteManufacturerSortById(int manufacturerSortId){
         serviceErrorHelper.findManufacturerSortOrElseThrow404(manufacturerSortId);
         productsRepository.setManufacturerSortToNull(manufacturerSortId );
